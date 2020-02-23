a1df086178c8c2c30cc72f88d8026eacf323f518
Lennethe <41440576+Lennethe@users.noreply.github.com>
2020-02-24 02:04:01 +0900

    Update E.cpp
    組み合わせの問題。
    空き部屋がiとなる人数の組み合わせは
    nCi * n-1Ci

--- Atcoder/ABC156/E.cpp
+++ Atcoder/ABC156/E.cpp
@@ -14,49 +14,51 @@ using namespace std;
 #define sortAlr(v) sort(v.begin(),v.end()); reverse(v.begin(),v.end())
 #define cout(n) cout<<fixed<<setprecision(n)
 
-ll ext_gcd(ll a, ll b, ll&x, ll&y){
+
+long long MOD = 1e9 + 7;
+std::vector<long long> func(2e5+2);
+void build_func(){
+  func[0] = 1;
+  func[1] = 1;
+  for(int i=2;i<2e5+2;i++)func[i] = func[i-1]*i%MOD;
+}
+
+template <typename T>
+T ext_gcd(T a, T b, T&x, T&y){
   if(b==0){
     x=1;y=0;return a;
   }
-  ll q=a/b;
-  ll g=ext_gcd(b,a-q*b,x,y);
-  ll z=x-q*y;
+  T q=a/b;
+  T g=ext_gcd(b,a-q*b,x,y);
+  T z=x-q*y;
   x=y;y=z;
   return g;
 }
 
 // aは割る言、mがmod
-ll invmod(ll a, ll m){
-  ll x,y;
+template <typename T>
+T invmod(T a, T m){
+  T x,y;
   ext_gcd(a,m,x,y);
   x%=m;
   if(x<0)x+=m;
   return x;
 }
 
+long long comb(long long n, long long k){
+  if(n <= k || n<=0 || k<=0) return 1;
+  long long x = func[n-k]; 
+  long long y = func[k]; 
+  return (long long)func[n] * invmod(x, MOD)%MOD * invmod(y, MOD)%MOD;
+}
+
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
+    build_func();
     ll n,k;
     cin>>n>>k;
-    ll MOD = 1e9+7;
-    vector<ll> fac(1e5+2);
-    fac[0] = 1;
-    fac[1] = 1;
-    FOR(i,2,1e5+2){
-        fac[i] = fac[i-1]*i%MOD;
-    }
-    k = min(n-1,k);
-    k++;
-
-    int a = n-1;
-    int b = k;
-    int c = n-k;
-    cout<<a<<" "<<b<<" "<<c<<endl;
-    cout<<endl;
-
-    cout<<fac[a+b]*invmod(fac[a],MOD)%MOD * invmod(fac[b], MOD)%MOD<<endl;
-    cout<<fac[a + c]%MOD * invmod(fac[a], MOD)%MOD * invmod(fac[c], MOD)%MOD<<endl;
-    ll ans = fac[a+b]*invmod(fac[a],MOD)%MOD * invmod(fac[b], MOD)%MOD * fac[a + c]%MOD * invmod(fac[a], MOD)%MOD * invmod(fac[c], MOD)%MOD;
+    ll ans = 0;
+    FOR(i,0,min(n,k+1))ans = (ans + comb(n,i)* comb(n-1,i))%MOD;
     cout<<ans<<endl;
 }
